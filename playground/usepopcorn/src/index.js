0ae189d7e0ac37380cf8b31cfb87279e83d0f0ea
Xu Li <xl1393@nyu.edu>
2024-04-19 16:32:11 -0400

    upddate

--- playground/usepopcorn/src/index.js
+++ playground/usepopcorn/src/index.js
@@ -3,13 +3,24 @@ import ReactDOM from 'react-dom/client';
 //import './index.css';
 //import App from './App';
 import StarRating from './StarRating';
+import { useState } from "react";
 
+
+function Test(){
+  const [movieRating, setMovieRating] = useState(0)
+  return(
+    <>
+    <StarRating color='blue' maxRating={10} onRating={setMovieRating} />
+    <p> the movie is rated at {movieRating}</p>
+    </>
+  )
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/*<App />*/}
-    <StarRating maxRating={10} />
-
+    <StarRating maxRating={5} messages={["terrible", 'bad','OK','good','amazing']} defaultRating={5} />
+  <Test />
   </React.StrictMode>
 );
 
